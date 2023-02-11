42bde413fa3104dd6f2099e4a9b7c276bbefd5e4
chanwoo <chocw0402@gmail.com>
2023-02-11 19:36:10 +0900

    initial commit

---
+++ src/main/java/backend/wal/WalApplication.java
@@ -0,0 +1,13 @@
+package backend.wal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/wal/WalApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.wal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
