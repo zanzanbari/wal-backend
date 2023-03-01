3fe0755f2c3ec4874fb96cd4a3d3230d26382f30
chanwoo <chocw0402@gmail.com>
2023-03-01 18:18:20 +0900

    test : WalSettingService 추가 메서드 테스트 - updateWalInfoByCancelCategoryTypes 제외 사유 : 현재 시간 비교 관련 메서드 테스트 리팩토링 예정

--- src/test/java/backend/wal/wal/service/WalSettingServiceTest.java
+++ src/test/java/backend/wal/wal/service/WalSettingServiceTest.java
@@ -10,6 +10,7 @@ import backend.wal.wal.repository.CategoryRepository;
 import backend.wal.wal.repository.ItemRepository;
 import backend.wal.wal.repository.TodayWalRepository;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -24,10 +26,13 @@ import java.util.stream.Stream;
 import static backend.wal.onboarding.domain.entity.WalCategoryType.*;
 import static backend.wal.onboarding.domain.entity.WalTimeType.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 @SpringBootTest
 @Transactional
 class WalSettingServiceTest {
+
+    private static final Long USER_ID = 1L;
 
     @Autowired
     private WalSettingService walSettingService;
@@ -46,7 +51,7 @@ class WalSettingServiceTest {
     @MethodSource("provideCategoryTypesAndExpectSize")
     void setNextWals(Set<WalCategoryType> categoryTypes, int expectSize) {
         // when
-        NextWals setNextWals = walSettingService.setNextWals(categoryTypes, 1L);
+        NextWals setNextWals = walSettingService.setNextWals(categoryTypes, USER_ID);
 
         // then
         assertThat(setNextWals.getSize()).isEqualTo(expectSize);
@@ -83,13 +88,13 @@ class WalSettingServiceTest {
         itemRepository.saveAll(List.of(comedyItem, fussItem, comfortItem, yellItem));
 
         Set<WalCategoryType> categoryTypes = Set.of(COMEDY, FUSS, COMFORT, YELL);
-        NextWals setNextWals = walSettingService.setNextWals(categoryTypes, 1L);
+        NextWals setNextWals = walSettingService.setNextWals(categoryTypes, USER_ID);
 
         // when
-        walSettingService.setTodayWals(timeTypes, 1L, setNextWals);
+        walSettingService.setTodayWals(timeTypes, USER_ID, setNextWals);
 
         // then
-        List<TodayWal> todayWals = todayWalRepository.findAllByUserId(1L);
+        List<TodayWal> todayWals = todayWalRepository.findTodayWalsByUserId(USER_ID);
         assertThat(todayWals).hasSize(expectSize);
     }
 
@@ -102,5 +107,25 @@ class WalSettingServiceTest {
                 Arguments.of(
                         Set.of(MORNING), 1)
         );
+    }
+
+    @DisplayName("수정 이후 취소된 카테고리 타입과 userId 를 받아 TodayWal 에서 삭제해야할 시간대를 반환한다")
+    @Test
+    void updateWalInfoByCancelCategoryTypes() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("비어있는 시간대와 추가해야하는 카테코리 타입이 없어도 updateWalInfoByAddCategoryTypesInEmptyTimeTypes 는 동작한다")
+    @Test
+    void updateWalInfoByAddCategoryTypesInEmptyTimeTypes() {
+        // when, then
+        assertThatCode(() ->
+                walSettingService
+                        .updateWalInfoByAddCategoryTypesInEmptyTimeTypes(new HashSet<>(), new HashSet<>(), USER_ID)
+        ).doesNotThrowAnyException();
     }
 }
