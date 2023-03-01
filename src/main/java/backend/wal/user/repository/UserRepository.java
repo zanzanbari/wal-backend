27dddf11740451b135805d81655b8b52b1ac3790
chanwoo <chocw0402@gmail.com>
2023-03-02 00:19:52 +0900

    add : existsUserBySocialInfoSocialIdAndSocialInfoSocialType 메서드 추가

--- src/main/java/backend/wal/user/repository/UserRepository.java
+++ src/main/java/backend/wal/user/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package backend.wal.user.repository;
 
+import backend.wal.user.domain.entity.SocialType;
 import backend.wal.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserById(Long userId);
+    boolean existsUserBySocialInfoSocialIdAndSocialInfoSocialType(String socialId, SocialType socialType);
 }
