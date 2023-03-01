0dc3b24640c2138ad1434c50cc0197061f6fbad6
chanwoo <chocw0402@gmail.com>
2023-03-01 14:37:15 +0900

    add : WalTimeType 필드, findAfterNow 메서드 추가

--- src/main/java/backend/wal/onboarding/domain/entity/WalTimeType.java
+++ src/main/java/backend/wal/onboarding/domain/entity/WalTimeType.java
@@ -3,14 +3,25 @@ package backend.wal.onboarding.domain.entity;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public enum WalTimeType {
 
-    MORNING("아침"),
-    AFTERNOON("점심"),
-    NIGHT("저녁"),
-    RESERVATION("예약"),
+    MORNING("아침", LocalTime.of(8, 0, 0)),
+    AFTERNOON("점심", LocalTime.of(14, 0, 0)),
+    NIGHT("저녁", LocalTime.of(20, 0, 0)),
+    RESERVATION("예약", null),
     ;
 
     private final String value;
+    private final LocalTime sendTime;
+
+    public WalTimeType findAfterNow(LocalDateTime now) {
+        LocalDateTime sendDateTime = LocalDateTime.of(now.toLocalDate(), this.sendTime);
+        return sendDateTime.isAfter(now)
+                ? this
+                : null;
+    }
 }
