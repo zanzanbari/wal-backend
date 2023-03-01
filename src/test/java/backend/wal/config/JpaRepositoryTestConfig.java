91b285bedf6b893bf932ace08a61e98b96f4e916
chanwoo <chocw0402@gmail.com>
2023-03-01 19:24:11 +0900

    feat : JpaRepositoryTestConfig 어노테이션 - in memory db (h2) 사용 시 AutoConfigureTestDatabase 설정 삭제 필요

---
+++ src/test/java/backend/wal/config/JpaRepositoryTestConfig.java
@@ -0,0 +1,16 @@
+package backend.wal.config;
+
+import backend.wal.config.querydsl.JPAConfig;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(JPAConfig.class)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JpaRepositoryTestConfig {
+}
