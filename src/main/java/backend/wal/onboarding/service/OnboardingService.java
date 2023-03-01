04ee484af3ad7ccc9810be2fcab49861bc726556
chanwoo <chocw0402@gmail.com>
2023-03-01 18:20:56 +0900

    add : OnboardingService 업데이트 메서드 추가 - updateOnboardTimeInfo, updateOnboardCategoryInfo

--- src/main/java/backend/wal/onboarding/service/OnboardingService.java
+++ src/main/java/backend/wal/onboarding/service/OnboardingService.java
@@ -1,7 +1,13 @@
 package backend.wal.onboarding.service;
 
+import backend.wal.advice.exception.NotFoundException;
+import backend.wal.onboarding.domain.WalTimeTypes;
 import backend.wal.onboarding.domain.entity.Onboarding;
+import backend.wal.onboarding.domain.entity.WalCategoryType;
+import backend.wal.onboarding.domain.entity.WalTimeType;
 import backend.wal.onboarding.dto.request.InitOnboardInfoRequestDto;
+import backend.wal.onboarding.dto.request.ModifyOnboardCategoryRequestDto;
+import backend.wal.onboarding.dto.request.ModifyOnboardTimeRequestDto;
 import backend.wal.onboarding.dto.response.OnboardInfoResponse;
 import backend.wal.onboarding.repository.OnboardingRepository;
 import backend.wal.user.repository.UserRepository;
@@ -12,6 +18,9 @@ import backend.wal.wal.service.WalSettingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -33,5 +42,57 @@ public class OnboardingService {
         walSettingService.setTodayWals(requestDto.getTimeTypes(), userId, setNextWals);
 
         return new OnboardInfoResponse(user.getNickname());
+    }
+
+    @Transactional
+    public void updateOnboardTimeInfo(ModifyOnboardTimeRequestDto requestDto, Long userId) {
+        Onboarding onboarding = findOnboardingBy(userId);
+        Set<WalTimeType> modifiedTimeTypes = requestDto.getTimeTypes();
+        Set<WalTimeType> canceledTimeTypes = onboarding.extractCancelTimeTypes(modifiedTimeTypes);
+        Set<WalTimeType> remainAfterCancel = onboarding.removeCanceledTimeTypes(modifiedTimeTypes);
+        Set<WalTimeType> addedTimeTypes = onboarding.extractAddTimeTypes(modifiedTimeTypes, remainAfterCancel);
+        onboarding.addTimes(addedTimeTypes);
+        updateWalTimeInfo(userId, canceledTimeTypes, addedTimeTypes);
+    }
+
+    private void updateWalTimeInfo(Long userId, Set<WalTimeType> canceledTimeTypes, Set<WalTimeType> addedTimeTypes) {
+        WalTimeTypes willCancelAfterNow = WalTimeTypes.createCompareAfterNow(canceledTimeTypes);
+        WalTimeTypes willAddAfterNow = WalTimeTypes.createCompareAfterNow(addedTimeTypes);
+        if (willCancelAfterNow.isExist()) {
+            walSettingService.updateTodayWalByCancelTimeTypes(willCancelAfterNow, userId);
+        }
+        if (willAddAfterNow.isExist()) {
+            walSettingService.updateTodayWalByAddTimeTypes(willAddAfterNow, userId);
+        }
+    }
+
+    @Transactional
+    public void updateOnboardCategoryInfo(ModifyOnboardCategoryRequestDto requestDto, Long userId) {
+        Onboarding onboarding = findOnboardingBy(userId);
+        Set<WalCategoryType> modifiedCategoryTypes = requestDto.getCategoryTypes();
+        Set<WalCategoryType> canceledCategoryTypes = onboarding.extractCancelCategoryTypes(modifiedCategoryTypes);
+        Set<WalCategoryType> remainAfterCancel = onboarding.removeCanceledCategoryTypes(modifiedCategoryTypes);
+        Set<WalCategoryType> addedCategoryTypes = onboarding.extractAddCategoryTypes(modifiedCategoryTypes, remainAfterCancel);
+        onboarding.addCategories(addedCategoryTypes);
+        updateWalCategoryInfo(userId, canceledCategoryTypes, addedCategoryTypes);
+    }
+
+    private void updateWalCategoryInfo(Long userId, Set<WalCategoryType> canceledCategoryTypes,
+                                       Set<WalCategoryType> addedCategoryTypes) {
+        Set<WalTimeType> willCancelAfterNow = new HashSet<>();
+        if (!canceledCategoryTypes.isEmpty()) {
+            willCancelAfterNow = walSettingService.updateWalInfoByCancelCategoryTypes(canceledCategoryTypes, userId);
+        }
+        if (!addedCategoryTypes.isEmpty()) {
+            walSettingService.updateWalInfoByAddCategoryTypesInEmptyTimeTypes(willCancelAfterNow, addedCategoryTypes, userId);
+        }
+    }
+
+    private Onboarding findOnboardingBy(Long userId) {
+        Onboarding onboarding = onboardingRepository.findByUserId(userId);
+        if (onboarding == null) {
+            throw new NotFoundException(String.format("유저 (%s)의 온보딩 정보가 존재하지 않습니다", userId));
+        }
+        return onboarding;
     }
 }
