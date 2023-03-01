3ecab82a7bfb8d562e4972a38e213b489cac5b30
chanwoo <chocw0402@gmail.com>
2023-03-01 15:45:11 +0900

    add : findByUserId 메서드 추가

--- src/main/java/backend/wal/onboarding/repository/OnboardingRepository.java
+++ src/main/java/backend/wal/onboarding/repository/OnboardingRepository.java
@@ -4,4 +4,5 @@ import backend.wal.onboarding.domain.entity.Onboarding;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OnboardingRepository extends JpaRepository<Onboarding, Long> {
+    Onboarding findByUserId(Long userId);
 }
