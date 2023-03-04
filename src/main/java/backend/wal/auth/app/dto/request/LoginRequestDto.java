5e1378ad3e74d99a7376dd391c5b282fb5bea423
chanwoo <chocw0402@gmail.com>
2023-03-05 00:28:32 +0900

    feat : LoginRequestDto 클래스 - service 레이어 dto

---
+++ src/main/java/backend/wal/auth/app/dto/request/LoginRequestDto.java
@@ -0,0 +1,31 @@
+package backend.wal.auth.app.dto.request;
+
+import backend.wal.user.domain.entity.SocialType;
+import backend.wal.user.dto.request.CreateUserDto;
+import lombok.Getter;
+
+@Getter
+public final class LoginRequestDto {
+
+    private final String socialAccessToken;
+    private final SocialType socialType;
+    private final String fcmToken;
+
+    public LoginRequestDto(final String socialAccessToken, final SocialType socialType, final String fcmToken) {
+        this.socialAccessToken = socialAccessToken;
+        this.socialType = socialType;
+        this.fcmToken = fcmToken;
+    }
+
+    public CreateUserDto toCreateUserDto(String nickname, String socialId) {
+        return CreateUserDto.builder()
+                .nickname(nickname)
+                .socialId(socialId)
+                .socialType(socialType)
+                .build();
+    }
+
+    public InitFcmRequestDto toFcmTokenServiceDto(Long userId) {
+        return new InitFcmRequestDto(userId, fcmToken);
+    }
+}
