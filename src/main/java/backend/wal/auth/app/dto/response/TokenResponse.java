0a37af443d3fea0934d84505768f6f97cf33049a
chanwoo <chocw0402@gmail.com>
2023-03-05 00:30:43 +0900

    feat : TokenResponse Dto - accessToken, refreshToken 응답 dto

---
+++ src/main/java/backend/wal/auth/app/dto/response/TokenResponse.java
@@ -0,0 +1,15 @@
+package backend.wal.auth.app.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public final class TokenResponse {
+
+    private final String accessToken;
+    private final String refreshToken;
+
+    public TokenResponse(final String accessToken, final String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
