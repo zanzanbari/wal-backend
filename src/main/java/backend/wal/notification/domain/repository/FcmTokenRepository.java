f415c06bba60fbf5909dfa46a2abf2e49ee8d541
chanwoo <chocw0402@gmail.com>
2023-03-11 01:40:06 +0900

    add : findFcmTokenByUserId 추가

--- src/main/java/backend/wal/notification/domain/repository/FcmTokenRepository.java
+++ src/main/java/backend/wal/notification/domain/repository/FcmTokenRepository.java
@@ -4,5 +4,8 @@ import backend.wal.notification.domain.entity.FcmToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
+
     boolean existsFcmTokenByUserId(Long userId);
+
+    FcmToken findFcmTokenByUserId(Long userId);
 }
