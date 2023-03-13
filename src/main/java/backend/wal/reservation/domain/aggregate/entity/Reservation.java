b2e483744477d120ae2cc11183d76c6c5559602d
chanwoo <chocw0402@gmail.com>
2023-03-13 15:34:21 +0900

    add : 생성자 추가, isToday 도메인 로직 추가

--- src/main/java/backend/wal/reservation/domain/aggregate/entity/Reservation.java
+++ src/main/java/backend/wal/reservation/domain/aggregate/entity/Reservation.java
@@ -1,14 +1,19 @@
-package backend.wal.reservation.domain.entity;
+package backend.wal.reservation.domain.aggregate.entity;
 
-import backend.wal.user.domain.aggregate.entity.User;
+import backend.wal.reservation.domain.aggregate.vo.SendStatus;
+import backend.wal.reservation.domain.aggregate.vo.ShowStatus;
+import backend.wal.reservation.app.dto.AddReservationRequestDto;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Reservation {
 
     @Id
@@ -16,12 +21,13 @@ public class Reservation {
     @Column(name = "reservation_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Column(nullable = false)
+    private Long userId;
 
+    @Column(nullable = false)
     private String message;
 
+    @Column(nullable = false)
     private LocalDateTime sendDueDate;
 
     @Enumerated(EnumType.STRING)
@@ -31,4 +37,29 @@ public class Reservation {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 10)
     private SendStatus sendStatus;
+
+    private Reservation(final Long userId, final String message, final LocalDateTime sendDueDate,
+                       final ShowStatus showStatus, final SendStatus sendStatus) {
+        this.userId = userId;
+        this.message = message;
+        this.sendDueDate = sendDueDate;
+        this.showStatus = showStatus;
+        this.sendStatus = sendStatus;
+    }
+
+    public static Reservation newInstance(final AddReservationRequestDto requestDto) {
+        LocalDateTime sendDueDate = LocalDateTime.of(requestDto.getSendDate(), requestDto.getSendTime());
+        return new Reservation(
+                requestDto.getUserId(),
+                requestDto.getMessage(),
+                sendDueDate,
+                requestDto.getShowStatus(),
+                SendStatus.NOT_DONE
+        );
+    }
+
+    public boolean isToday(LocalDate today) {
+        return sendDueDate.toLocalDate()
+                .isEqual(today);
+    }
 }
