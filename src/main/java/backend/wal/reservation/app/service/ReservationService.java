3769f9ebf99e9de5b90117ebde46cb96ef81afdc
chanwoo <chocw0402@gmail.com>
2023-03-13 20:09:27 +0900

    feat : ReservationService 클래스

---
+++ src/main/java/backend/wal/reservation/app/service/ReservationService.java
@@ -0,0 +1,46 @@
+package backend.wal.reservation.app.service;
+
+import backend.wal.reservation.app.dto.AddReservationRequestDto;
+import backend.wal.reservation.domain.ReservationTime;
+import backend.wal.reservation.domain.aggregate.entity.Reservation;
+import backend.wal.reservation.domain.repository.ReservationRepository;
+import backend.wal.reservation.exception.ConflictReservationException;
+import backend.wal.wal.service.TodayWalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+    private final TodayWalService todayWalService;
+    private final Clock clock;
+
+    @Transactional
+    public void register(AddReservationRequestDto requestDto) {
+        ReservationTime reservationTime = ReservationTime.startDayFrom(requestDto.getSendDate());
+        validateReservationDate(reservationTime, requestDto.getUserId());
+        Reservation reservation = reservationRepository.save(Reservation.newInstance(requestDto));
+        registerTodayWalIfToday(reservation);
+    }
+
+    private void validateReservationDate(ReservationTime reservationTime, Long userId) {
+        LocalDateTime startDay = reservationTime.getReservationDate();
+        LocalDateTime endDay = reservationTime.getNextDate();
+        if (reservationRepository.existsReservationBySendDueDateBetweenAndUserId(startDay, endDay, userId)) {
+            throw ConflictReservationException.already(startDay.toLocalDate());
+        }
+    }
+
+    private void registerTodayWalIfToday(Reservation reservation) {
+        if (reservation.isToday(LocalDate.now(clock))) {
+            todayWalService.registerReservationTodayWal(reservation.getUserId(), reservation.getMessage());
+        }
+    }
+}
