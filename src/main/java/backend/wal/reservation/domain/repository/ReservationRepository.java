c9daed69acd33fbf8c44c15b37d188a3ca4a194d
chanwoo <chocw0402@gmail.com>
2023-03-18 01:35:53 +0900

    add : findReservationsByUserId, findReservationsBySendDueDateAfterAndUserId 메서드 추가

--- src/main/java/backend/wal/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/backend/wal/reservation/domain/repository/ReservationRepository.java
@@ -4,7 +4,13 @@ import backend.wal.reservation.domain.aggregate.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
     boolean existsReservationBySendDueDateBetweenAndUserId(LocalDateTime start, LocalDateTime end, Long userId);
+
+    List<Reservation> findReservationsByUserId(Long userId);
+
+    List<Reservation> findReservationsBySendDueDateAfterAndUserId(LocalDateTime now, Long userId);
 }
