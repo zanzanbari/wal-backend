de81773fda524a7145b81d8daa18ef0e5386bcfb
chanwoo <chocw0402@gmail.com>
2023-03-23 20:19:14 +0900

    style : 띄어쓰기 수정

--- src/main/java/backend/wal/user/domain/repository/UserRepository.java
+++ src/main/java/backend/wal/user/domain/repository/UserRepository.java
@@ -5,6 +5,7 @@ import backend.wal.user.domain.aggregate.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findUserById(Long userId);
 
     boolean existsUserBySocialInfoSocialIdAndSocialInfoSocialType(String socialId, SocialType socialType);
