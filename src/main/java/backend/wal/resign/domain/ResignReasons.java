0b861dd3549ccf2d42af1719890a72aa685d9d51
chanwoo <chocw0402@gmail.com>
2023-03-23 20:10:05 +0900

    feat : ResignReasons 클래스

---
+++ src/main/java/backend/wal/resign/domain/ResignReasons.java
@@ -0,0 +1,18 @@
+package backend.wal.resign.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Set;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+public class ResignReasons {
+
+    private Set<ResignReason> reasons;
+
+    public ResignReasons(final Set<ResignReason> reasons) {
+        this.reasons = reasons;
+    }
+}
