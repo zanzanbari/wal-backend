213cb94989acaf0febe853417dc6dbf098b2d2c9
chanwoo <chocw0402@gmail.com>
2023-03-26 23:04:19 +0900

    refactor : ReservationScheduledFutures 로 기능 대체

--- src/main/java/backend/wal/reservation/domain/ReservationScheduler.java
+++ src/main/java/backend/wal/reservation/domain/ReservationScheduler.java
@@ -4,10 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.concurrent.*;
-
-import static java.util.concurrent.TimeUnit.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public final class ReservationScheduler {
@@ -15,19 +15,19 @@ public final class ReservationScheduler {
     private static final Logger LOGGER = LoggerFactory.getLogger(ReservationScheduler.class);
 
     private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-    private final Map<Long, ScheduledFuture<?>> reservationScheduledFutures = new ConcurrentHashMap<>();
+    private final ReservationScheduledFutures reservationScheduledFutures = new ReservationScheduledFutures();
 
     public void sendMessageAfterDelay(Runnable task, long delayTime, Long reservationId) {
-        ScheduledFuture<?> scheduledFuture = scheduledExecutorService.schedule(task, delayTime, MILLISECONDS);
-        reservationScheduledFutures.put(reservationId, scheduledFuture);
+        ScheduledFuture<?> scheduledFuture = scheduledExecutorService.schedule(task, delayTime, TimeUnit.MILLISECONDS);
+        reservationScheduledFutures.register(reservationId, scheduledFuture);
         LOGGER.debug("예약번호 {}번 등록 완료 - 딜레이 시간 {}", reservationId, delayTime);
     }
 
     public void cancelMessage(Long reservationId) {
-        ScheduledFuture<?> scheduledFuture = reservationScheduledFutures.get(reservationId);
-        scheduledFuture.cancel(true);
-        reservationScheduledFutures.remove(reservationId);
-        LOGGER.debug("예약번호 {}번 취소 완료 - 남은 딜레이 시간 {}", reservationId, scheduledFuture.getDelay(MILLISECONDS));
+        if (reservationScheduledFutures.cancelScheduleByKey(reservationId)) {
+            reservationScheduledFutures.removeByKey(reservationId);
+            LOGGER.debug("예약번호 {}번 취소 완료", reservationId);
+        }
     }
 
     public void shoutDown() {
