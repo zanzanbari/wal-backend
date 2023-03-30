791163deaf7e073b0bbf139fabd4108d14a07de1
chanwoo <chocw0402@gmail.com>
2023-03-31 02:13:28 +0900

    add : retrieveReservationBetweenTodayAndTomorrow 메서드 추가

--- src/main/java/backend/wal/reservation/application/port/in/ReservationRetrieveUseCase.java
+++ src/main/java/backend/wal/reservation/application/port/in/ReservationRetrieveUseCase.java
@@ -1,13 +1,17 @@
 package backend.wal.reservation.application.port.in;
 
+import backend.wal.reservation.domain.aggregate.Reservation;
 import backend.wal.reservation.web.dto.ReservationCalendarResponse;
 import backend.wal.reservation.web.dto.ReservationHistoryResponse;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservationRetrieveUseCase {
 
     ReservationHistoryResponse retrieveReservationHistory(Long userId);
 
     List<ReservationCalendarResponse> retrieveReservationDate(Long userId);
+
+    Optional<Reservation> retrieveReservationBetweenTodayAndTomorrow(Long userId);
 }
