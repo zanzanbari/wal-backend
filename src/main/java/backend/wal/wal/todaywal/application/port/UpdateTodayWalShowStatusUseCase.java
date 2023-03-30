ec150c8fed80f1a0788c81cf21f695446a828569
chanwoo <chocw0402@gmail.com>
2023-03-30 16:12:54 +0900

    refactor : 클래스, 인터페이스 명 변경

--- src/main/java/backend/wal/wal/todaywal/application/port/UpdateTodayWalShowStatusUseCase.java
+++ src/main/java/backend/wal/wal/todaywal/application/port/UpdateTodayWalShowStatusUseCase.java
@@ -1,12 +1,6 @@
 package backend.wal.wal.todaywal.application.port;
 
-import backend.wal.wal.todaywal.domain.aggregate.TodayWal;
-
-import java.util.List;
-
-public interface TodayWalUseCase {
-
-    List<TodayWal> findTodayWalsByUserId(Long userId);
+public interface UpdateTodayWalShowStatusUseCase {
 
     void updateShowStatus(Long todayWalId, Long userId);
 }
--- src/main/java/backend/wal/wal/todaywal/application/service/UpdateTodayWalShowStatusService.java
+++ src/main/java/backend/wal/wal/todaywal/application/service/UpdateTodayWalShowStatusService.java
@@ -1,30 +1,23 @@
 package backend.wal.wal.todaywal.application.service;
 
-import backend.wal.wal.todaywal.application.port.TodayWalUseCase;
+import backend.wal.wal.todaywal.application.port.UpdateTodayWalShowStatusUseCase;
 import backend.wal.wal.todaywal.domain.repository.TodayWalRepository;
 import backend.wal.wal.todaywal.domain.aggregate.TodayWal;
 import backend.wal.wal.todaywal.exception.NotFoundTodayWalException;
 import backend.wal.support.annotation.AppService;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @AppService
-@Transactional(readOnly = true)
-public class TodayWalService implements TodayWalUseCase {
+public class UpdateTodayWalShowStatusService implements UpdateTodayWalShowStatusUseCase {
 
     private final TodayWalRepository todayWalRepository;
 
-    public TodayWalService(final TodayWalRepository todayWalRepository) {
+    public UpdateTodayWalShowStatusService(final TodayWalRepository todayWalRepository) {
         this.todayWalRepository = todayWalRepository;
     }
 
     @Override
-    public List<TodayWal> findTodayWalsByUserId(Long userId) {
-        return todayWalRepository.findTodayWalsByUserId(userId);
-    }
-
-    @Override
+    @Transactional
     public void updateShowStatus(Long todayWalId, Long userId) {
         TodayWal todayWal = todayWalRepository.findTodayWalByIdAndUserId(todayWalId, userId)
                 .orElseThrow(() -> NotFoundTodayWalException.notExist(todayWalId, userId));
