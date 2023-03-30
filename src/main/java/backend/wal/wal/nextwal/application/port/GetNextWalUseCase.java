6e2709377d426ea4d9545e1336fdab5885951767
chanwoo <chocw0402@gmail.com>
2023-03-31 02:22:55 +0900

    feat : GetNextWalUseCase 인터페이스

---
+++ src/main/java/backend/wal/wal/nextwal/application/port/GetNextWalUseCase.java
@@ -0,0 +1,11 @@
+package backend.wal.wal.nextwal.application.port;
+
+import backend.wal.wal.nextwal.domain.NextWals;
+import backend.wal.wal.nextwal.domain.aggregate.NextWal;
+
+public interface GetNextWalUseCase {
+
+    NextWal getRandomNextWal(NextWals nextWals);
+
+    NextWals getNextWalsByUserId(Long userId);
+}
