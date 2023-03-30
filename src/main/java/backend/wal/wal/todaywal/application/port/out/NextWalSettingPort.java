b659829253309446878d892a561d4f8e852192de
chanwoo <chocw0402@gmail.com>
2023-03-31 02:17:08 +0900

    feat : NextWalSettingPort 인터페이스 - NextWal UseCase 호출을 위한 인터페이스

---
+++ src/main/java/backend/wal/wal/todaywal/application/port/out/NextWalSettingPort.java
@@ -0,0 +1,12 @@
+package backend.wal.wal.todaywal.application.port.out;
+
+import backend.wal.wal.common.domain.WalCategoryType;
+import backend.wal.wal.nextwal.domain.NextWals;
+import backend.wal.wal.nextwal.domain.aggregate.NextWal;
+
+public interface NextWalSettingPort {
+
+    NextWal getRandomNextWal(NextWals nextWals);
+
+    void updateNextWal(NextWals nextWals, NextWal randomNextWal, WalCategoryType categoryType);
+}
