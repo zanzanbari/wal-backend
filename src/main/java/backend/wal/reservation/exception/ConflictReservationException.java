fb9669292fadca4c44b46a20cc83a129022d5a30
chanwoo <chocw0402@gmail.com>
2023-04-01 02:24:00 +0900

    none 메서드 이동 및 NotFoundReservationException 클래스 구현

--- src/main/java/backend/wal/reservation/exception/ConflictReservationException.java
+++ src/main/java/backend/wal/reservation/exception/ConflictReservationException.java
@@ -13,8 +13,4 @@ public final class ConflictReservationException extends ConflictException {
     public static ConflictException alreadyExistDate(LocalDate sendDate) {
         return new ConflictReservationException(String.format("이미 존재하는 예약 날짜 (%s) 입니다", sendDate));
     }
-
-    public static ConflictException none() {
-        return new ConflictReservationException("유저의 예약한 왈소리가 없습니다");
-    }
 }
