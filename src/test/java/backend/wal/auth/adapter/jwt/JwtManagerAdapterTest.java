0bf1a24089985ea291ba86a422ea14a8852049ed
chanwoo <chocw0402@gmail.com>
2023-03-31 21:24:40 +0900

    test : createRefreshToken 메서드 테스트 추가

--- src/test/java/backend/wal/auth/adapter/jwt/JwtManagerAdapterTest.java
+++ src/test/java/backend/wal/auth/adapter/jwt/JwtManagerAdapterTest.java
@@ -1,8 +1,8 @@
-package backend.wal.auth.support.token;
+package backend.wal.auth.adapter.jwt;
 
+import backend.wal.auth.application.port.out.CreateRefreshTokenResponseDto;
 import backend.wal.auth.application.port.out.JwtManagerPort;
 import backend.wal.auth.exception.UnAuthorizedTokenException;
-import backend.wal.auth.adapter.jwt.JwtManagerAdapter;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -40,6 +40,24 @@ class JwtManagerAdapterTest {
                 .parseClaimsJws(accessToken)
                 .getBody();
         assertThat(body.get("USER_ID", Long.class)).isEqualTo(USER_ID);
+    }
+
+    @DisplayName("유저의 id 를 받아 refreshToken 을 생성하면 정해진 리프레시 토큰 만료시간을 갖는 refreshToken 이 설정된다")
+    @Test
+    void createRefreshToken() {
+        // given
+        Date refreshExpired = new Date(new Date().getTime() + REFRESH_TOKEN_EXPIRES_IN) ;
+
+        // when
+        CreateRefreshTokenResponseDto createTokenDto = jwtManager.createRefreshToken(USER_ID);
+
+        // then
+        Claims body = Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(createTokenDto.getTokenValue())
+                .getBody();
+        assertThat(body.getExpiration()).isBeforeOrEqualTo(refreshExpired);
     }
 
     @DisplayName("userId 값으로 발행된 accessToken 값으로 부터 유저의 아이디 값을 가져온다")
