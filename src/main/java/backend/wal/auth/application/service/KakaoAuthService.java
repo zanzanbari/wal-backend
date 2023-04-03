1981c8d4a3a75b74a180eb656a40037b7a5aa273
chanwoo <chocw0402@gmail.com>
2023-04-03 16:05:39 +0900

    refactor : AppleAuthService, KakaoAuthService 도메인 서비스를 이용한 리팩토링

--- src/main/java/backend/wal/auth/application/service/KakaoAuthService.java
+++ src/main/java/backend/wal/auth/application/service/KakaoAuthService.java
@@ -2,42 +2,32 @@ package backend.wal.auth.application.service;
 
 import backend.wal.auth.application.port.in.LoginRequestDto;
 import backend.wal.auth.application.port.in.AuthUseCase;
+import backend.wal.auth.application.port.in.LoginResponseDto;
 import backend.wal.auth.application.port.out.OAuthUserInfoResponseDto;
 import backend.wal.auth.application.port.out.OAuthApiClientPort;
-import backend.wal.auth.application.port.out.RegisterFcmPort;
-import backend.wal.auth.application.port.out.UserPort;
-import backend.wal.auth.exception.UnAuthorizedUserException;
-import backend.wal.user.domain.aggregate.entity.User;
+import backend.wal.auth.domain.service.OAuthDomainService;
 import backend.wal.support.annotation.AppService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.transaction.annotation.Transactional;
 
 @AppService
 public class KakaoAuthService implements AuthUseCase {
 
-    private final OAuthApiClientPort oAuthApiClientPort;
-    private final UserPort userPort;
-    private final RegisterFcmPort registerFcmPort;
+    private static final String KAKAO_OAUTH_CLIENT_PORT = "kakaoApiClientAdapter";
 
-    public KakaoAuthService(final OAuthApiClientPort oAuthApiClientPort, final UserPort userPort,
-                            final RegisterFcmPort registerFcmPort) {
+    private final OAuthApiClientPort oAuthApiClientPort;
+    private final OAuthDomainService oAuthDomainService;
+
+    public KakaoAuthService(@Qualifier(KAKAO_OAUTH_CLIENT_PORT) final OAuthApiClientPort oAuthApiClientPort,
+                            final OAuthDomainService oAuthDomainService) {
         this.oAuthApiClientPort = oAuthApiClientPort;
-        this.userPort = userPort;
-        this.registerFcmPort = registerFcmPort;
+        this.oAuthDomainService = oAuthDomainService;
     }
 
     @Override
     @Transactional
-    public Long login(LoginRequestDto requestDto) {
+    public LoginResponseDto login(LoginRequestDto requestDto) {
         OAuthUserInfoResponseDto oAuthUserInfo = oAuthApiClientPort.getOAuthUserId(requestDto.getSocialAccessToken());
-        User alreadyUser = userPort.findSocialUserCall(oAuthUserInfo.getId(), requestDto.getSocialType());
-        if (alreadyUser == null) {
-            Long newUserId = userPort.signupCall(requestDto.toCreateUserDto(oAuthUserInfo.getNickname(), oAuthUserInfo.getId()));
-            registerFcmPort.registerCall(requestDto.toFcmTokenServiceDto(newUserId));
-            return newUserId;
-        }
-        if (alreadyUser.isDeleted()) {
-            throw UnAuthorizedUserException.resignUser();
-        }
-        return alreadyUser.getId();
+        return oAuthDomainService.signupOrLogin(requestDto, oAuthUserInfo);
     }
 }
