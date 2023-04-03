ce9c5bb581314b3b47ac872e0e52391df8d2b144
chanwoo <chocw0402@gmail.com>
2023-04-03 15:31:34 +0900

    add : getPublicKeys 요청 메서드 추가

--- src/main/java/backend/wal/auth/adapter/oauth/apple/AppleAuthApiClient.java
+++ src/main/java/backend/wal/auth/adapter/oauth/apple/AppleAuthApiClient.java
@@ -1,7 +1,12 @@
 package backend.wal.auth.adapter.oauth.apple;
 
+import backend.wal.auth.adapter.oauth.apple.dto.ApplePublicKeyResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(value = "AppleAuthApiClient", url = "https://appleid.apple.com/auth")
 public interface AppleAuthApiClient {
+
+    @GetMapping("/keys")
+    ApplePublicKeyResponse getPublicKeys();
 }
