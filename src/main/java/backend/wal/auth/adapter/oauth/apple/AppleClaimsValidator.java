85f4a818cf20be510367af15f522e32e2dcf6480
chanwoo <chocw0402@gmail.com>
2023-04-05 19:48:53 +0900

    refactor : @PropertySource 추가 및 hasRightIssAndClientId, isValidExp 인자 수정

--- src/main/java/backend/wal/auth/adapter/oauth/apple/AppleClaimsValidator.java
+++ src/main/java/backend/wal/auth/adapter/oauth/apple/AppleClaimsValidator.java
@@ -1,31 +1,32 @@
-package backend.wal.auth.adapter.jwt;
+package backend.wal.auth.adapter.oauth.apple;
 
-import io.jsonwebtoken.Claims;
+import backend.wal.support.YamlPropertySourceFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-
 @Component
-public final class AppleJwtClaimValidator {
+@PropertySource(
+        value = "classpath:wal-backend-config/application-apple.yml",
+        factory = YamlPropertySourceFactory.class,
+        ignoreResourceNotFound = true)
+public final class AppleClaimsValidator {
 
     private final String iss;
     private final String clientId;
 
-    public AppleJwtClaimValidator(@Value("${oauth.apple.iss}") String iss,
-                                  @Value("${oauth.apple.client-id}") String clientId) {
+    public AppleClaimsValidator(@Value("${oauth.apple.iss}") String iss,
+                                @Value("${oauth.apple.client-id}") String clientId) {
         this.iss = iss;
         this.clientId = clientId;
     }
 
     public boolean hasRightIssAndClientId(String issuer, String audience) {
-        return claims.getIssuer().equals(iss)
-                && claims.getAudience().equals(clientId);
+        return issuer.equals(iss)
+                && audience.equals(clientId);
     }
 
-    public boolean isValidExp(Claims claims) {
-        Date expiration = claims.getExpiration();
-        Date now = new Date();
-        return expiration.after(now);
+    public boolean isValidExp(long expiration, long now) {
+        return expiration > now;
     }
 }
