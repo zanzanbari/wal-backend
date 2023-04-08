4032f8e4436a8329031a1222f58506013e7dde44
chanwoo <chocw0402@gmail.com>
2023-04-09 01:24:55 +0900

    add : FeignExceptionDecoder 적용

--- src/main/java/backend/wal/auth/adapter/oauth/apple/AppleAuthApiClient.java
+++ src/main/java/backend/wal/auth/adapter/oauth/apple/AppleAuthApiClient.java
@@ -1,11 +1,16 @@
 package backend.wal.auth.adapter.oauth.apple;
 
 import backend.wal.auth.adapter.oauth.apple.dto.ApplePublicKeyResponse;
+import backend.wal.auth.config.FeignExceptionDecoder;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "AppleAuthApiClient", url = "https://appleid.apple.com/auth")
+@FeignClient(
+        value = "AppleAuthApiClient",
+        url = "https://appleid.apple.com/auth",
+        configuration = FeignExceptionDecoder.class
+)
 public interface AppleAuthApiClient {
 
     @GetMapping("/keys")
