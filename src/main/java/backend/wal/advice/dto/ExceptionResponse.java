6a48383490978dc3373edffcca4516cc38b5f8cf
chanwoo <chocw0402@gmail.com>
2023-04-09 01:19:34 +0900

    fix : getter 메서드 추가 - JacksonHttp2MessageConverter 직렬화 오류 수정

--- src/main/java/backend/wal/advice/dto/ExceptionResponse.java
+++ src/main/java/backend/wal/advice/dto/ExceptionResponse.java
@@ -2,13 +2,19 @@ package backend.wal.advice.dto;
 
 public class ExceptionResponse {
 
+    private final int statusCode;
     private final String message;
 
-    private ExceptionResponse(final String message) {
+    public ExceptionResponse(final int statusCode, final String message) {
+        this.statusCode = statusCode;
         this.message = message;
     }
 
-    public static ExceptionResponse create(final String message) {
-        return new ExceptionResponse(message);
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
