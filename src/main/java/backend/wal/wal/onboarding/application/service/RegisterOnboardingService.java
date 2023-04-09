624d1e926c3a72a1f054b2c45e1b4b9733d5fbdc
chanwoo <chocw0402@gmail.com>
2023-04-09 19:21:11 +0900

    refactor : AppService 레이어 @Transactioanl 제거

--- src/main/java/backend/wal/wal/onboarding/application/service/RegisterOnboardingService.java
+++ src/main/java/backend/wal/wal/onboarding/application/service/RegisterOnboardingService.java
@@ -6,8 +6,6 @@ import backend.wal.wal.onboarding.application.port.out.WalSettingPort;
 import backend.wal.wal.onboarding.domain.service.OnboardingDomainService;
 import backend.wal.support.annotation.AppService;
 
-import org.springframework.transaction.annotation.Transactional;
-
 @AppService
 public class RegisterOnboardingService implements RegisterOnboardingUseCase {
 
@@ -20,7 +18,6 @@ public class RegisterOnboardingService implements RegisterOnboardingUseCase {
     }
 
     @Override
-    @Transactional
     public void registerOnboardInfo(InitOnboardInfoRequestDto requestDto, Long userId) {
         onboardingDomainService.register(requestDto.toOnboardingEntity(userId));
         walSettingPort.setWalInfo(requestDto.getTimeTypes(), requestDto.getCategoryTypes(), userId);
