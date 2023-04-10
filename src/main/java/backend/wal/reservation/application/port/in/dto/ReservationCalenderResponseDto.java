ef8c956130c6d5763e602da1ab76bbe9c01a806a
chanwoo <chocw0402@gmail.com>
2023-04-10 14:40:38 +0900

    refactor : 클래스명 변경

--- src/main/java/backend/wal/reservation/application/port/in/dto/ReservationCalenderResponseDto.java
+++ src/main/java/backend/wal/reservation/application/port/in/dto/ReservationCalenderResponseDto.java
@@ -3,15 +3,19 @@ package backend.wal.reservation.application.port.in.dto;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public final class ReservationCalendarResponseDto {
+public final class ReservationCalenderResponseDto {
 
     private final LocalDate reserveDate;
 
-    public ReservationCalendarResponseDto(final LocalDate reserveDate) {
+    public ReservationCalenderResponseDto(final LocalDate reserveDate) {
         this.reserveDate = reserveDate;
     }
 
-    public static ReservationCalendarResponseDto create(final LocalDateTime localDateTime) {
-        return new ReservationCalendarResponseDto(localDateTime.toLocalDate());
+    public static ReservationCalenderResponseDto create(final LocalDateTime localDateTime) {
+        return new ReservationCalenderResponseDto(localDateTime.toLocalDate());
+    }
+
+    public LocalDate getReserveDate() {
+        return reserveDate;
     }
 }
