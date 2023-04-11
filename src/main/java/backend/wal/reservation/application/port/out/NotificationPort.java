641f45efe5d2941fb7de0cd9297b62b2b054990d
chanwoo <chocw0402@gmail.com>
2023-04-11 22:42:43 +0900

    refactor : sendCall 파라미터 변경

--- src/main/java/backend/wal/reservation/application/port/out/NotificationPort.java
+++ src/main/java/backend/wal/reservation/application/port/out/NotificationPort.java
@@ -2,5 +2,5 @@ package backend.wal.reservation.application.port.out;
 
 public interface NotificationPort {
 
-    void sendCall(Long userId, String message);
+    void sendCall(NotificationRequestDto requestDto);
 }
