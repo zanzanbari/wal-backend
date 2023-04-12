86331d3e4b02f2071aadde4a3a19f25c53af5688
chanwoo <chocw0402@gmail.com>
2023-04-12 20:37:04 +0900

    refactor : properties 재설정

--- src/test/java/backend/wal/config/JpaRepositoryTestConfig.java
+++ src/test/java/backend/wal/config/JpaRepositoryTestConfig.java
@@ -1,21 +1,20 @@
 package backend.wal.config;
 
 import backend.wal.config.jpa.JPAConfig;
-import backend.wal.support.YamlPropertySourceFactory;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@PropertySource(
-        value = "classpath:wal-backend-config/application-test.yml",
-        factory = YamlPropertySourceFactory.class,
-        ignoreResourceNotFound = true)
-@DataJpaTest(properties = {"spring.config.additional-location=classpath:wal-backend-config/application-test.yml"})
+@DataJpaTest(properties = {
+        "spring.datasource.url=jdbc:mysql://localhost:3306/test",
+        "spring.datasource.username=test",
+        "spring.datasource.password=test_password",
+        "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver"
+})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(JPAConfig.class)
 @Retention(RetentionPolicy.RUNTIME)
