d5dbbe3f8f6ab6ad2e0009e507ec380cb3177c8a
chanwoo <chocw0402@gmail.com>
2023-04-13 15:44:40 +0900

    feat : health check controller

---
+++ src/main/java/backend/wal/health/HealthCheckController.java
@@ -0,0 +1,14 @@
+package backend.wal.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("hello ");
+    }
+}
