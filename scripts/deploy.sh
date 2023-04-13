98d31c4e062bfadb5e32fe4507ffe4719fe947af
chanwoo <chocw0402@gmail.com>
2023-04-13 16:00:33 +0900

    cd : deploy 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,3 @@ cp $ORIGINAL_JAR_FILE $ROOT_PATH
 cd $ROOT_PATH
 nohup java -jar -Dspring.profiles.active=prod /home/ubuntu/wal-server/build/libs/*.jar > /home/ubuntu/nohup.out 2>&1 &
 echo "$CURRENT_TIME > jar 파일 실행"
-
-EXECUTED_PROCESS_PID=$(lsof -t -i tcp:8080)
-echo "$CURRENT_TIME > executed process pid = $EXECUTED_PROCESS_PID"
-curl -s -d "payload={\"text\":\"Application Execution : $EXECUTED_PROCESS_PID\"}" "${SLACK_WEBHOOK_URI}"
