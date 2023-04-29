92919f0ac5f1e7e9a46239d3d666cee2802bbf90
chanwoo <chocw0402@gmail.com>
2023-04-29 18:56:59 +0900

    add : findNextWalsWithItemByUserId, updateNextWalItem 메서드 추가 - findNextWalsWithItemByUserId : Item 필드 Lazy loading 으로 인한 오류 fetch join 으로 수정 - updateNextWalItem : NextWal 엔티티의 Item 필드 프록시 객체 로딩으로 인한 update 오류 수정

--- src/main/java/backend/wal/wal/nextwal/domain/repository/NextWalRepository.java
+++ src/main/java/backend/wal/wal/nextwal/domain/repository/NextWalRepository.java
@@ -1,9 +1,12 @@
 package backend.wal.wal.nextwal.domain.repository;
 
 import backend.wal.wal.common.domain.WalCategoryType;
+import backend.wal.wal.nextwal.domain.aggregate.Item;
 import backend.wal.wal.nextwal.domain.aggregate.NextWal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Set;
@@ -13,4 +16,11 @@ public interface NextWalRepository extends JpaRepository<NextWal, Long> {
     List<NextWal> findNextWalsByUserId(Long userId);
 
     List<NextWal> findNextWalsByCategoryTypeInAndUserId(Set<WalCategoryType> categoryTypes, Long userId);
+
+    @Query("SELECT nw FROM NextWal nw JOIN FETCH nw.item")
+    List<NextWal> findNextWalsWithItemByUserId(Long userId);
+
+    @Modifying
+    @Query("UPDATE NextWal nw SET nw.item = :item WHERE nw.id = :nextWalId")
+    void updateNextWalItem(Long nextWalId, Item item);
 }
