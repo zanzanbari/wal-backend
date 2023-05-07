213080108c1fe0eb9710d2e4516e4991f7970f4a
chanwoo <chocw0402@gmail.com>
2023-05-07 19:17:15 +0900

    add : findRefreshTokenByUserId 메서드 추가

--- src/main/java/backend/wal/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/backend/wal/auth/domain/repository/RefreshTokenRepository.java
@@ -7,5 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findRefreshTokenByUserId(Long userId);
+
     Optional<RefreshToken> findRefreshTokenByValue(String refreshToken);
 }
