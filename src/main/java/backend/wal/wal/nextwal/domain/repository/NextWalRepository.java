2e040b643e904dc1cb551297695c0bd984c532c1
chanwoo <chocw0402@gmail.com>
2023-05-08 14:00:13 +0900

    fix : update 쿼리 메서드 제거

--- src/main/java/backend/wal/wal/nextwal/domain/repository/NextWalRepository.java
+++ src/main/java/backend/wal/wal/nextwal/domain/repository/NextWalRepository.java
@@ -1,11 +1,9 @@
 package backend.wal.wal.nextwal.domain.repository;
 
 import backend.wal.wal.common.domain.WalCategoryType;
-import backend.wal.wal.nextwal.domain.aggregate.Item;
 import backend.wal.wal.nextwal.domain.aggregate.NextWal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -19,8 +17,4 @@ public interface NextWalRepository extends JpaRepository<NextWal, Long> {
 
     @Query("SELECT nw FROM NextWal nw JOIN FETCH nw.item")
     List<NextWal> findNextWalsWithItemByUserId(Long userId);
-
-    @Modifying
-    @Query("UPDATE NextWal nw SET nw.item = :item WHERE nw.id = :nextWalId")
-    void updateNextWalItem(Long nextWalId, Item item);
 }
