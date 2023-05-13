1ce872c24556b2fe7e62023f3531de0d31bbcce8
chanwoo <chocw0402@gmail.com>
2023-05-13 14:17:01 +0900

    add : update 유스 케이스 구현

--- src/main/java/backend/wal/notification/application/service/FcmTokenService.java
+++ src/main/java/backend/wal/notification/application/service/FcmTokenService.java
@@ -1,19 +1,21 @@
 package backend.wal.notification.application.service;
 
+import backend.wal.notification.application.port.in.FcmTokenUseCase;
 import backend.wal.notification.application.port.in.InitFcmRequestDto;
-import backend.wal.notification.application.port.in.RegisterFcmTokenUseCase;
+import backend.wal.notification.application.port.in.UpdateFcmTokenRequestDto;
 import backend.wal.notification.domain.FcmToken;
 import backend.wal.notification.domain.repository.FcmTokenRepository;
+import backend.wal.notification.exception.NotFoundFcmTokenException;
 import backend.wal.support.annotation.AppService;
 
 import org.springframework.transaction.annotation.Transactional;
 
 @AppService
-public class RegisterFcmTokenService implements RegisterFcmTokenUseCase {
+public class FcmTokenService implements FcmTokenUseCase {
 
     private final FcmTokenRepository fcmTokenRepository;
 
-    public RegisterFcmTokenService(final FcmTokenRepository fcmTokenRepository) {
+    public FcmTokenService(final FcmTokenRepository fcmTokenRepository) {
         this.fcmTokenRepository = fcmTokenRepository;
     }
 
@@ -22,6 +24,16 @@ public class RegisterFcmTokenService implements RegisterFcmTokenUseCase {
     public void register(InitFcmRequestDto requestDto) {
         if (!fcmTokenRepository.existsFcmTokenByUserId(requestDto.getUserId())) {
             fcmTokenRepository.save(FcmToken.newInstance(requestDto.getUserId(), requestDto.getFcmToken()));
+        }
+    }
+
+    @Override
+    @Transactional
+    public void update(UpdateFcmTokenRequestDto requestDto) {
+        FcmToken fcmToken = fcmTokenRepository.findFcmTokenByUserId(requestDto.getUserId())
+                .orElseThrow(() -> NotFoundFcmTokenException.notExists(requestDto.getUserId()));
+        if (fcmToken.isNotSameWith(requestDto.getFcmToken())) {
+            fcmToken.updateTokenValue(requestDto.getFcmToken());
         }
     }
 }
