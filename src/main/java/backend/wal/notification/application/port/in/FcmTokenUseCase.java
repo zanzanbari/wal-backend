74290b55ff34ae695cb57e07671a8c9766be09bd
chanwoo <chocw0402@gmail.com>
2023-05-13 14:16:23 +0900

    add : update use case 추가

--- src/main/java/backend/wal/notification/application/port/in/FcmTokenUseCase.java
+++ src/main/java/backend/wal/notification/application/port/in/FcmTokenUseCase.java
@@ -1,6 +1,8 @@
 package backend.wal.notification.application.port.in;
 
-public interface RegisterFcmTokenUseCase {
+public interface FcmTokenUseCase {
 
     void register(InitFcmRequestDto requestDto);
+
+    void update(UpdateFcmTokenRequestDto requestDto);
 }
