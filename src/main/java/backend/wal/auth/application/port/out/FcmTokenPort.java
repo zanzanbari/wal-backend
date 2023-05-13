8caae6410956f46c6f2f443bb5fb7d4c0b51538e
chanwoo <chocw0402@gmail.com>
2023-05-13 14:18:27 +0900

    add : checkAndUpdateCall 유스 케이스 추가

--- src/main/java/backend/wal/auth/application/port/out/FcmTokenPort.java
+++ src/main/java/backend/wal/auth/application/port/out/FcmTokenPort.java
@@ -1,8 +1,11 @@
 package backend.wal.auth.application.port.out;
 
 import backend.wal.notification.application.port.in.InitFcmRequestDto;
+import backend.wal.notification.application.port.in.UpdateFcmTokenRequestDto;
 
-public interface RegisterFcmPort {
+public interface FcmTokenPort {
 
     void registerCall(InitFcmRequestDto requestDto);
+
+    void checkAndUpdateCall(UpdateFcmTokenRequestDto requestDto);
 }
