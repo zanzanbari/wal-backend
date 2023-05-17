b2b540502cc3db6a948c318013032453e43d2142
chanwoo <chocw0402@gmail.com>
2023-05-17 15:07:39 +0900

    feat : ScheduledMessage 클래스

---
+++ src/main/java/backend/wal/reservation/domain/aggregate/ScheduledMessage.java
@@ -0,0 +1,70 @@
+package backend.wal.reservation.domain.aggregate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Entity
+public class ScheduledMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long reservationId;
+
+    private Long userId;
+
+    private String message;
+
+    private LocalDateTime sendDueDate;
+
+    protected ScheduledMessage() {
+    }
+
+    private ScheduledMessage(final Long reservationId, final Long userId,
+                            final String message, final LocalDateTime sendDueDate) {
+        this.reservationId = reservationId;
+        this.userId = userId;
+        this.message = message;
+        this.sendDueDate = sendDueDate;
+    }
+
+    public static ScheduledMessage newInstance(final Reservation reservation) {
+        return new ScheduledMessage(
+                reservation.getId(),
+                reservation.getUserId(),
+                reservation.getMessage(),
+                reservation.getSendDueDate()
+        );
+    }
+
+    public long getDelayTimeAboutNow(LocalDateTime now) {
+        return Duration.between(now, sendDueDate)
+                .toMillis();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getSendDueDate() {
+        return sendDueDate;
+    }
+}
