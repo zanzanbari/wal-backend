eaaeb09b7a283e833301c4a993eaf52dcf2519e7
chanwoo <chocw0402@gmail.com>
2023-06-13 17:46:02 +0900

    test : ReservationScheduledFutures 테스트 수정

--- src/test/java/backend/wal/reservation/adapter/ReservationScheduledFuturesTest.java
+++ src/test/java/backend/wal/reservation/adapter/ReservationScheduledFuturesTest.java
@@ -1,29 +1,35 @@
 package backend.wal.reservation.adapter;
 
+import backend.wal.reservation.exception.NotFoundReservationException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
 import java.util.concurrent.*;
 
 import static java.util.concurrent.TimeUnit.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ReservationScheduledFuturesTest {
 
     private static final Long RESERVATION_KEY = 1L;
 
     private final ReservationScheduledFutures reservationScheduledFutures = new ReservationScheduledFutures();
-    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-    private final ScheduledFuture<?> scheduledFuture = executorService.schedule(() -> {}, 1, MILLISECONDS);
 
     @DisplayName("예약 key 값과 수행될 scheduledFuture 을 받아 등록한다")
     @Test
     void register() {
+        // given
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        ScheduledFuture<?> scheduledFuture = executorService.schedule(() -> {}, 1, MILLISECONDS);
+
         // when
         reservationScheduledFutures.register(RESERVATION_KEY, scheduledFuture);
 
         // then
-        ScheduledFuture<?> actual = reservationScheduledFutures.findByKey(RESERVATION_KEY);
+        ScheduledFuture<?> actual = reservationScheduledFutures.findByKey(RESERVATION_KEY).get();
         assertThat((Future<?>) actual).isEqualTo(scheduledFuture);
     }
 
@@ -46,6 +52,8 @@ class ReservationScheduledFuturesTest {
     @Test
     void removeByKey() {
         // given
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        ScheduledFuture<?> scheduledFuture = executorService.schedule(() -> {}, 1, MILLISECONDS);
         reservationScheduledFutures.register(RESERVATION_KEY, scheduledFuture);
         reservationScheduledFutures.cancelScheduleByKey(RESERVATION_KEY);
 
@@ -53,7 +61,17 @@ class ReservationScheduledFuturesTest {
         reservationScheduledFutures.removeByKey(RESERVATION_KEY);
 
         // then
-        ScheduledFuture<?> actual = reservationScheduledFutures.findByKey(RESERVATION_KEY);
-        assertThat((Future<?>) actual).isNull();
+        Optional<ScheduledFuture<?>> actual = reservationScheduledFutures.findByKey(RESERVATION_KEY);
+        assertThat(actual).isNotPresent();
+    }
+
+
+    @DisplayName("예약 key 이 없다면 에러가 발생한다")
+    @Test
+    void fail_cancelScheduleByKey() {
+        // when, then
+        assertThatThrownBy(() -> reservationScheduledFutures.cancelScheduleByKey(RESERVATION_KEY))
+                .isInstanceOf(NotFoundReservationException.class)
+                .hasMessage(String.format("존재하지 않거나 이미 삭제된 히스토리 (%s) 입니다", RESERVATION_KEY));
     }
 }
