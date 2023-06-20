3f1ddedac0eac86bf4c9af4dc7433a66dcc329b1
chanwoo <chocw0402@gmail.com>
2023-06-20 17:17:59 +0900

    fix : ResignUserSchedulerAdapter 제거

--- src/main/java/backend/wal/user/application/service/ResignUserService.java
+++ src/main/java/backend/wal/user/application/service/ResignUserService.java
@@ -1,6 +1,5 @@
 package backend.wal.user.application.service;
 
-import backend.wal.user.application.port.out.ResignUserSchedulerPort;
 import backend.wal.user.application.port.in.ResignUserUseCase;
 import backend.wal.user.domain.aggregate.entity.User;
 import backend.wal.user.domain.repository.UserRepository;
@@ -11,21 +10,15 @@ import org.springframework.transaction.annotation.Transactional;
 @AppService
 public class ResignUserService implements ResignUserUseCase {
 
-    private static final long MILLIS_OF_ONE_DAY = 1000 * 60 * 60 * 24;
-
     private final UserRepository userRepository;
-    private final ResignUserSchedulerPort resignUserSchedulerPort;
 
-    public ResignUserService(final UserRepository userRepository, final ResignUserSchedulerPort resignUserSchedulerPort) {
+    public ResignUserService(final UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.resignUserSchedulerPort = resignUserSchedulerPort;
     }
 
     @Transactional
     public void resign(Long userId) {
         User user = UserServiceUtils.findExistsUserByUserId(userRepository, userId);
         user.resign();
-
-        resignUserSchedulerPort.resignAfterDay(() -> userRepository.delete(user), MILLIS_OF_ONE_DAY);
     }
 }
