ca22c57207681b89998b10c1c09d72b0060694db
chanwoo <chocw0402@gmail.com>
2023-07-06 00:55:36 +0900

    fix : push noti 로직 변경

--- src/main/java/backend/wal/scheduler/WalNotificationScheduler.java
+++ src/main/java/backend/wal/scheduler/WalNotificationScheduler.java
@@ -1,14 +1,12 @@
 package backend.wal.scheduler;
 
+import backend.wal.notification.application.port.in.NotificationTimeRequestDto;
 import backend.wal.notification.application.port.in.NotificationUseCase;
 import backend.wal.wal.common.domain.WalTimeType;
 import backend.wal.wal.onboarding.domain.repository.OnboardingTimeRepository;
-import backend.wal.wal.todaywal.domain.aggregate.TodayWal;
-import backend.wal.wal.todaywal.domain.repository.TodayWalRepository;
 
 import org.springframework.stereotype.Component;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,14 +17,11 @@ public class WalNotificationScheduler {
 
     private final NotificationUseCase notificationUseCase;
     private final OnboardingTimeRepository onboardingTimeRepository;
-    private final TodayWalRepository todayWalRepository;
 
     public WalNotificationScheduler(final NotificationUseCase notificationUseCase,
-                                    final OnboardingTimeRepository onboardingTimeRepository,
-                                    final TodayWalRepository todayWalRepository) {
+                                    final OnboardingTimeRepository onboardingTimeRepository) {
         this.notificationUseCase = notificationUseCase;
         this.onboardingTimeRepository = onboardingTimeRepository;
-        this.todayWalRepository = todayWalRepository;
     }
 
     @Scheduled(cron = "0 0 8 * * *")
@@ -44,10 +39,8 @@ public class WalNotificationScheduler {
         executePushNotification(NIGHT);
     }
 
-    @Transactional
     public void executePushNotification(WalTimeType timeType) {
         List<Long> userIds = onboardingTimeRepository.findUserIdsByTimeType(timeType);
-        List<TodayWal> todayWals = todayWalRepository.findTodayWalByUserIdInAndTimeType(userIds, timeType);
-        todayWals.forEach(todayWal -> notificationUseCase.sendMessage(todayWal.getUserId(), todayWal.getMessage()));
+        notificationUseCase.sendMessage(new NotificationTimeRequestDto(userIds));
     }
 }
