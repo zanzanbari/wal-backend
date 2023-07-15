eb719af6b29c90d2374ad9dac4c049b7e9c6a824
chanwoo <chocw0402@gmail.com>
2023-07-15 14:52:11 +0900

    fix : sendMessage 파라미터 변경

--- src/main/java/backend/wal/notification/application/port/out/FirebaseMessagingPort.java
+++ src/main/java/backend/wal/notification/application/port/out/FirebaseMessagingPort.java
@@ -1,10 +1,8 @@
 package backend.wal.notification.application.port.out;
 
-import java.util.List;
-
 public interface FirebaseMessagingPort {
 
-    void sendMessage(List<String> fcmTokenValues);
+    void sendMessage(NotificationRequestDtos requestDtos);
 
     void sendReservation(String fcmTokenValue, Long reservationId, String content);
 }
