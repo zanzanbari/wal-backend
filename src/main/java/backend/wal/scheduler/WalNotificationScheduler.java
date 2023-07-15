b14d77283dc20d40036f935830aafaa1d61e15cf
chanwoo <chocw0402@gmail.com>
2023-07-15 14:38:17 +0900

    refactor : PushNotificationRepository, FirebaseMessagingPort 를 바로 연결해서 알림 전송

--- src/main/java/backend/wal/scheduler/WalNotificationScheduler.java
+++ src/main/java/backend/wal/scheduler/WalNotificationScheduler.java
@@ -1,27 +1,29 @@
 package backend.wal.scheduler;
 
-import backend.wal.notification.application.port.in.NotificationTimeRequestDto;
-import backend.wal.notification.application.port.in.NotificationUseCase;
+import backend.wal.notification.application.port.out.FirebaseMessagingPort;
+import backend.wal.notification.application.port.out.NotificationRequestDto;
+import backend.wal.notification.application.port.out.NotificationRequestDtos;
+import backend.wal.scheduler.repository.PushNotificationRepository;
 import backend.wal.wal.common.domain.WalTimeType;
-import backend.wal.wal.onboarding.domain.repository.OnboardingTimeRepository;
 
 import org.springframework.stereotype.Component;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static backend.wal.wal.common.domain.WalTimeType.*;
 
 @Component
 public class WalNotificationScheduler {
 
-    private final NotificationUseCase notificationUseCase;
-    private final OnboardingTimeRepository onboardingTimeRepository;
+    private final FirebaseMessagingPort firebaseMessagingPort;
+    private final PushNotificationRepository pushNotificationRepository;
 
-    public WalNotificationScheduler(final NotificationUseCase notificationUseCase,
-                                    final OnboardingTimeRepository onboardingTimeRepository) {
-        this.notificationUseCase = notificationUseCase;
-        this.onboardingTimeRepository = onboardingTimeRepository;
+    public WalNotificationScheduler(final FirebaseMessagingPort firebaseMessagingPort,
+                                    final PushNotificationRepository pushNotificationRepository) {
+        this.firebaseMessagingPort = firebaseMessagingPort;
+        this.pushNotificationRepository = pushNotificationRepository;
     }
 
     @Scheduled(cron = "0 0 8 * * *")
@@ -40,7 +42,15 @@ public class WalNotificationScheduler {
     }
 
     public void executePushNotification(WalTimeType timeType) {
-        List<Long> userIds = onboardingTimeRepository.findUserIdsByTimeType(timeType);
-        notificationUseCase.sendMessage(new NotificationTimeRequestDto(userIds));
+        List<NotificationRequestDto> requestDtos = pushNotificationRepository
+                .findTodayWalMessageWithUserIdByTimeType(timeType)
+                .stream()
+                .map(todayWalContentsAndFcmTokenValue ->
+                        new NotificationRequestDto(
+                                todayWalContentsAndFcmTokenValue.getFcmToken(),
+                                todayWalContentsAndFcmTokenValue.getMessage())
+                )
+                .collect(Collectors.toUnmodifiableList());
+        firebaseMessagingPort.sendMessage(new NotificationRequestDtos(requestDtos));
     }
 }
