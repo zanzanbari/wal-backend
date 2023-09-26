a464538c962b1f315c7dd979a07d27acb1d81782
chanwoo <chocw0402@gmail.com>
2023-09-26 16:53:16 +0900

    feat : UpdateCensorItemInfoRequest 클래스

---
+++ src/main/java/backend/wal/admin/web/dto/UpdateCensorItemInfoRequest.java
@@ -0,0 +1,28 @@
+package backend.wal.admin.web.dto;
+
+
+import backend.wal.wal.censorWal.domain.aggregate.CheckStatus;
+
+import javax.validation.constraints.NotNull;
+
+public class UpdateCensorItemInfoRequest {
+
+    @NotNull
+    private Long censorItemId;
+
+    @NotNull
+    private CheckStatus checkStatus;
+
+    public UpdateCensorItemInfoRequest(Long censorItemId, CheckStatus checkStatus) {
+        this.censorItemId = censorItemId;
+        this.checkStatus = checkStatus;
+    }
+
+    public Long getCensorItemId() {
+        return censorItemId;
+    }
+
+    public CheckStatus getCheckStatus() {
+        return checkStatus;
+    }
+}
