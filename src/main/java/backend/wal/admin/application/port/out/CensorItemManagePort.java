716dd503416abb93d018f16e7523eab3aaf5113b
chanwoo <chocw0402@gmail.com>
2023-09-26 16:50:46 +0900

    feat : CensorItemManagePort 인터페이스

---
+++ src/main/java/backend/wal/admin/application/port/out/CensorItemManagePort.java
@@ -0,0 +1,14 @@
+package backend.wal.admin.application.port.out;
+
+import backend.wal.admin.web.dto.RetrieveCensorItemResponse;
+import backend.wal.admin.web.dto.RetrieveCensorItemRequest;
+import backend.wal.wal.censorWal.domain.aggregate.CheckStatus;
+
+import java.util.List;
+
+public interface CensorItemManagePort {
+
+    List<RetrieveCensorItemResponse> retrieveCensorItemInfo(RetrieveCensorItemRequest request);
+
+    void updateCheckStatus(Long censorItemId, CheckStatus checkStatus);
+}
