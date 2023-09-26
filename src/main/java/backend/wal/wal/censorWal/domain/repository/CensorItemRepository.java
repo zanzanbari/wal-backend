125c9054ca750fee0edef677f469214ec1e11cb3
chanwoo <chocw0402@gmail.com>
2023-09-26 16:21:05 +0900

    feat : JpaRepository 인터페이스를 구현한 CensorItemRepository

---
+++ src/main/java/backend/wal/wal/censorWal/domain/repository/CensorItemRepository.java
@@ -0,0 +1,21 @@
+package backend.wal.wal.censorWal.domain.repository;
+
+import backend.wal.wal.censorWal.domain.aggregate.CensorItem;
+import backend.wal.wal.censorWal.domain.aggregate.CheckStatus;
+import backend.wal.wal.common.domain.WalCategoryType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CensorItemRepository extends JpaRepository<CensorItem, Long> {
+
+    List<CensorItem> findAllByCheckStatus(WalCategoryType categoryType, CheckStatus checkStatus);
+
+    List<CensorItem> findAllByIdIn(List<Long> censorItemIds);
+
+    @Modifying
+    @Query("UPDATE CensorItem c SET c.checkStatus =: checkStatus WHERE c.id =: censorItemId")
+    void updateCheckStatus( Long censorItemId, CheckStatus checkStatus);
+}
