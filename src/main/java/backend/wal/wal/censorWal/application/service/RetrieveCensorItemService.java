bc037418da6f25c95bc20c59ea3a6850be780ae2
chanwoo <chocw0402@gmail.com>
2023-09-27 17:30:23 +0900

    refactor : CheckStatus static import

--- src/main/java/backend/wal/wal/censorWal/application/service/RetrieveCensorItemService.java
+++ src/main/java/backend/wal/wal/censorWal/application/service/RetrieveCensorItemService.java
@@ -1,16 +1,18 @@
 package backend.wal.wal.censorWal.application.service;
 
 import backend.wal.support.annotation.AppService;
-import backend.wal.wal.censorWal.application.port.in.dto.ApprovedCensorItemResponseDto;
-import backend.wal.wal.censorWal.domain.aggregate.CheckStatus;
-import backend.wal.wal.censorWal.application.port.in.dto.UncheckedCensorItemResponseDto;
-import backend.wal.wal.censorWal.application.port.in.dto.RetrieveCensorItemRequestDto;
 import backend.wal.wal.censorWal.application.port.in.RetrieveCensorItemUseCase;
+import backend.wal.wal.censorWal.application.port.in.dto.ApprovedCensorItemResponseDto;
+import backend.wal.wal.censorWal.application.port.in.dto.RetrieveCensorItemRequestDto;
+import backend.wal.wal.censorWal.application.port.in.dto.UncheckedCensorItemResponseDto;
 import backend.wal.wal.censorWal.domain.repository.CensorItemRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static backend.wal.wal.censorWal.domain.aggregate.CheckStatus.APPROVED;
+import static backend.wal.wal.censorWal.domain.aggregate.CheckStatus.UNCHECKED;
 
 @AppService
 @Transactional(readOnly = true)
@@ -26,7 +28,7 @@ public class RetrieveCensorItemService implements RetrieveCensorItemUseCase {
     public List<UncheckedCensorItemResponseDto> retrieveUncheckedCensorItemInfo(
             RetrieveCensorItemRequestDto requestDto) {
         return censorItemRepository
-                .findAllByCategoryTypeAndCheckStatus(requestDto.getCategoryType(), CheckStatus.UNCHECKED)
+                .findAllByCategoryTypeAndCheckStatus(requestDto.getCategoryType(), UNCHECKED)
                 .stream()
                 .map(censorItem -> new UncheckedCensorItemResponseDto(
                         censorItem.getId(),
@@ -40,7 +42,7 @@ public class RetrieveCensorItemService implements RetrieveCensorItemUseCase {
     public List<ApprovedCensorItemResponseDto> retrieveApprovedCensorItemInfo(
             RetrieveCensorItemRequestDto requestDto) {
         return censorItemRepository
-                .findAllByCategoryTypeAndCheckStatus(requestDto.getCategoryType(), CheckStatus.APPROVED)
+                .findAllByCategoryTypeAndCheckStatus(requestDto.getCategoryType(), APPROVED)
                 .stream()
                 .map(censorItem -> new ApprovedCensorItemResponseDto(
                         censorItem.getCategoryType(),
