efb525ce5a0d5951c3eac0e0b410853da927925d
chanwoo <chocw0402@gmail.com>
2023-09-27 17:21:53 +0900

    feat : AdminRegisterItemManageController 클래스 - Item 등록 컨트롤러

---
+++ src/main/java/backend/wal/admin/web/AdminRegisterItemManageController.java
@@ -0,0 +1,29 @@
+package backend.wal.admin.web;
+
+import backend.wal.admin.application.port.out.RegisterItemManagePort;
+import backend.wal.admin.web.dto.RetrieveCensorItemRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/admin/item")
+public class AdminRegisterItemManageController {
+
+    private final RegisterItemManagePort registerItemManagePort;
+
+    public AdminRegisterItemManageController(final RegisterItemManagePort registerItemManagePort) {
+        this.registerItemManagePort = registerItemManagePort;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@Valid @RequestBody RetrieveCensorItemRequest request) {
+        registerItemManagePort.registerItemsBy(request.getCategoryType());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
