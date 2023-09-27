e032d7c89ffda518adf5d4226d13132fbf3f7745
chanwoo <chocw0402@gmail.com>
2023-09-27 17:05:50 +0900

    add : retrieveApprovedCensorItemInfo 메서드 추가

--- src/main/java/backend/wal/wal/censorWal/application/port/in/RetrieveCensorItemUseCase.java
+++ src/main/java/backend/wal/wal/censorWal/application/port/in/RetrieveCensorItemUseCase.java
@@ -1,11 +1,14 @@
 package backend.wal.wal.censorWal.application.port.in;
 
+import backend.wal.wal.censorWal.application.port.in.dto.ApprovedCensorItemResponseDto;
 import backend.wal.wal.censorWal.application.port.in.dto.RetrieveCensorItemRequestDto;
-import backend.wal.wal.censorWal.application.port.in.dto.RetrieveCensorItemResponseDto;
+import backend.wal.wal.censorWal.application.port.in.dto.UncheckedCensorItemResponseDto;
 
 import java.util.List;
 
 public interface RetrieveCensorItemUseCase {
 
-    List<RetrieveCensorItemResponseDto> retrieveCensorItemInfo(RetrieveCensorItemRequestDto requestDto);
+    List<UncheckedCensorItemResponseDto> retrieveUncheckedCensorItemInfo(RetrieveCensorItemRequestDto requestDto);
+
+    List<ApprovedCensorItemResponseDto> retrieveApprovedCensorItemInfo(RetrieveCensorItemRequestDto requestDto);
 }
