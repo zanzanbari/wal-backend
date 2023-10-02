439647f94dfc6961a31f2a415a05c76a31ac1350
chanwoo <chocw0402@gmail.com>
2023-10-02 13:44:31 +0900

    feat : AdminAuthUseCase 인터페이스를 구현한 AdminAuthService

---
+++ src/main/java/backend/wal/admin/application/service/AdminAuthService.java
@@ -0,0 +1,51 @@
+package backend.wal.admin.application.service;
+
+import backend.wal.admin.application.port.in.AdminAuthUseCase;
+import backend.wal.admin.application.port.in.AdminLoginRequestDto;
+import backend.wal.admin.application.port.out.PasswordManagePort;
+import backend.wal.admin.domain.aggregate.Admin;
+import backend.wal.admin.domain.repository.AdminRepository;
+import backend.wal.admin.exception.NotFoundAdminException;
+import backend.wal.admin.exception.UnAuthorizedPasswordException;
+import backend.wal.support.annotation.AppService;
+import org.springframework.transaction.annotation.Transactional;
+
+@AppService
+public class AdminAuthService implements AdminAuthUseCase {
+
+    private final AdminRepository adminRepository;
+    private final PasswordManagePort passwordManagePort;
+
+    public AdminAuthService(final AdminRepository adminRepository, final PasswordManagePort passwordManagePort) {
+        this.adminRepository = adminRepository;
+        this.passwordManagePort = passwordManagePort;
+    }
+
+    @Override
+    @Transactional
+    public void create(AdminLoginRequestDto requestDto) {
+        adminRepository.save(new Admin(
+                requestDto.getEmail(),
+                passwordManagePort.encode(requestDto.getPassword())
+        ));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Long login(AdminLoginRequestDto requestDto) {
+        Admin admin = findExistAdminByEmail(requestDto.getEmail());
+        validateMatchPassword(requestDto.getPassword(), admin.getPassword());
+        return admin.getId();
+    }
+
+    private Admin findExistAdminByEmail(String email) {
+        return adminRepository.findByEmail(email)
+                .orElseThrow(() -> NotFoundAdminException.notExists(email));
+    }
+
+    private void validateMatchPassword(String requestPassword, String adminPassword) {
+        if (!passwordManagePort.isMatch(requestPassword, adminPassword)) {
+            throw UnAuthorizedPasswordException.wrong();
+        }
+    }
+}
