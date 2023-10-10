40f6ff3432ce3e4d5765070b0602e7aa3b895a0a
chanwoo <chocw0402@gmail.com>
2023-10-11 01:04:01 +0900

    refactor : JwtManagerAdpater 의존성 -> JwtManagerPort 의존성으로 교체

--- src/main/java/backend/wal/auth/web/support/interceptor/ReissueTokenInterceptor.java
+++ src/main/java/backend/wal/auth/web/support/interceptor/ReissueTokenInterceptor.java
@@ -1,6 +1,6 @@
 package backend.wal.auth.web.support.interceptor;
 
-import backend.wal.auth.adapter.jwt.JwtManagerAdapter;
+import backend.wal.auth.application.port.out.JwtManagerPort;
 import backend.wal.support.utils.HttpHeaderUtils;
 
 import org.springframework.stereotype.Component;
@@ -13,17 +13,17 @@ import java.util.Objects;
 @Component
 public final class ReissueTokenInterceptor extends TokenInterceptor {
 
-    private final JwtManagerAdapter jwtManagerAdapter;
+    private final JwtManagerPort jwtManagerPort;
 
-    public ReissueTokenInterceptor(final JwtManagerAdapter jwtManagerAdapter) {
-        this.jwtManagerAdapter = jwtManagerAdapter;
+    public ReissueTokenInterceptor(final JwtManagerPort jwtManagerPort) {
+        this.jwtManagerPort = jwtManagerPort;
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String refreshToken = Objects.requireNonNull(request).getHeader(HttpHeaderUtils.REFRESH_TOKEN);
         checkHeaderHasToken(refreshToken);
-        jwtManagerAdapter.validateToken(refreshToken);
+        jwtManagerPort.validateToken(refreshToken);
         request.setAttribute("Refresh-Token", refreshToken);
         return true;
     }
