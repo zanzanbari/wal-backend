90a711f7e8319849a26ca1dd74098e17a722e4b0
chanwoo <chocw0402@gmail.com>
2023-11-18 15:33:13 +0900

    add : JPAQueryManageInterceptor profile local 시 추가

--- src/main/java/backend/wal/config/web/WebConfig.java
+++ src/main/java/backend/wal/config/web/WebConfig.java
@@ -4,7 +4,9 @@ import backend.wal.auth.web.support.interceptor.AuthenticationInterceptor;
 import backend.wal.auth.web.support.interceptor.ReissueTokenInterceptor;
 import backend.wal.auth.web.support.resolver.LoginUserResolver;
 import backend.wal.auth.web.support.resolver.RefreshTokenResolver;
+import backend.wal.support.query.JPAQueryManageInterceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -17,15 +19,18 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final AuthenticationInterceptor authenticationInterceptor;
     private final ReissueTokenInterceptor reissueTokenInterceptor;
+    private final JPAQueryManageInterceptor jpaQueryManageInterceptor;
     private final LoginUserResolver loginUserResolver;
     private final RefreshTokenResolver refreshTokenResolver;
 
     public WebConfig(final AuthenticationInterceptor authenticationInterceptor,
                      final ReissueTokenInterceptor reissueTokenInterceptor,
+                     final @Autowired(required = false) JPAQueryManageInterceptor jpaQueryManageInterceptor,
                      final LoginUserResolver loginUserResolver,
                      final RefreshTokenResolver refreshTokenResolver) {
         this.authenticationInterceptor = authenticationInterceptor;
         this.reissueTokenInterceptor = reissueTokenInterceptor;
+        this.jpaQueryManageInterceptor = jpaQueryManageInterceptor;
         this.loginUserResolver = loginUserResolver;
         this.refreshTokenResolver = refreshTokenResolver;
     }
@@ -41,6 +46,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/profile");
         registry.addInterceptor(reissueTokenInterceptor)
                 .addPathPatterns("/v2/auth/reissue");
+        if (jpaQueryManageInterceptor != null) {
+            registry.addInterceptor(jpaQueryManageInterceptor)
+                    .addPathPatterns("/**");
+        }
     }
 
     @Override
