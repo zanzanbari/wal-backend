54dfd290dbb701134416215f12d71e2415aeddd7
chanwoo <chocw0402@gmail.com>
2023-11-18 15:31:29 +0900

    feat : HandlerInterceptor 를 구현한 JPAQueryManageInterceptor

---
+++ src/main/java/backend/wal/support/query/JPAQueryManageInterceptor.java
@@ -0,0 +1,37 @@
+package backend.wal.support.query;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JPAQueryManageInterceptor implements HandlerInterceptor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JPAQueryManageInterceptor.class);
+
+    private final JPAQueryInspector jpaQueryInspector;
+
+    public JPAQueryManageInterceptor(JPAQueryInspector jpaQueryInspector) {
+        this.jpaQueryInspector = jpaQueryInspector;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        jpaQueryInspector.start();
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        JPAQueryInspector.QueryInspectResult queryInspectResult = jpaQueryInspector.inspectResult();
+        LOGGER.info("METHOD: [{}], URI: {}, QUERY_COUNT: {}, QUERY_EXECUTION_TIME: {} ms",
+                request.getMethod(),
+                request.getRequestURI(),
+                queryInspectResult.getCount(),
+                queryInspectResult.getTime())
+        ;
+        jpaQueryInspector.finish();
+    }
+}
