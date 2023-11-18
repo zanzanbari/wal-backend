28ff7e854c02d077fe3b349dfbf9e3b29bcda0fc
chanwoo <chocw0402@gmail.com>
2023-11-18 15:32:27 +0900

    feat : JPAQueryManageConfig - local, test profile 에만 동작하도록 Bean 등록

---
+++ src/main/java/backend/wal/support/query/JPAQueryManageConfig.java
@@ -0,0 +1,15 @@
+package backend.wal.support.query;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class JPAQueryManageConfig {
+
+    @Bean
+    @Profile({"local", "test"})
+    public JPAQueryManageInterceptor jpaQueryManageInterceptor() {
+        return new JPAQueryManageInterceptor(new JPAQueryInspector());
+    }
+}
