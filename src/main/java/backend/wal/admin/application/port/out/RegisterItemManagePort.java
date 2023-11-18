1752465f061ea7084257c6bdbeb23069dda50674
chanwoo <chocw0402@gmail.com>
2023-11-18 19:28:31 +0900

    add : registerAllItems 메시지 추가

--- src/main/java/backend/wal/admin/application/port/out/RegisterItemManagePort.java
+++ src/main/java/backend/wal/admin/application/port/out/RegisterItemManagePort.java
@@ -2,6 +2,11 @@ package backend.wal.admin.application.port.out;
 
 import backend.wal.wal.common.domain.WalCategoryType;
 
+import java.util.Set;
+
 public interface RegisterItemManagePort {
+
     void registerItemsBy(WalCategoryType categoryType);
+
+    void registerAllItems(Set<String> contents, WalCategoryType categoryType);
 }
