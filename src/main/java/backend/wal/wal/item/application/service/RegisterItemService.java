4fc4bed42316392baf7c208df8433020c92c8053
chanwoo <chocw0402@gmail.com>
2023-11-28 14:40:05 +0900

    refactor : ItemRepository import -> ItemPersistencePort

--- src/main/java/backend/wal/wal/item/application/service/RegisterItemService.java
+++ src/main/java/backend/wal/wal/item/application/service/RegisterItemService.java
@@ -4,38 +4,23 @@ import backend.wal.support.annotation.AppService;
 import backend.wal.wal.common.domain.WalCategoryType;
 import backend.wal.wal.item.application.port.in.RegisterItemRequestDto;
 import backend.wal.wal.item.application.port.in.RegisterItemUseCase;
-import backend.wal.wal.item.domain.aggregate.Category;
-import backend.wal.wal.item.domain.aggregate.Item;
-import backend.wal.wal.item.domain.repository.CategoryRepository;
-import backend.wal.wal.item.domain.repository.ItemRepository;
+import backend.wal.wal.item.application.port.out.ItemPersistencePort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @AppService
 public class RegisterItemService implements RegisterItemUseCase {
 
-    private final ItemRepository itemRepository;
-    private final CategoryRepository categoryRepository;
+    private final ItemPersistencePort itemPersistencePort;
 
-    public RegisterItemService(final ItemRepository itemRepository, final CategoryRepository categoryRepository) {
-        this.itemRepository = itemRepository;
-        this.categoryRepository = categoryRepository;
+    public RegisterItemService(ItemPersistencePort itemPersistencePort) {
+        this.itemPersistencePort = itemPersistencePort;
     }
 
     @Override
     @Transactional
     public void registerNewItems(List<RegisterItemRequestDto> requestDtos, WalCategoryType categoryType) {
-        Category category = categoryRepository.findByCategoryType(categoryType);
-        List<Item> newItems = requestDtos.stream()
-                .map(requestDto -> new Item(
-                        category,
-                        requestDto.getContents(),
-                        requestDto.getImageUrl(),
-                        requestDto.getCategoryItemNumber())
-                )
-                .collect(Collectors.toUnmodifiableList());
-        itemRepository.saveAll(newItems);
+        itemPersistencePort.saveAll(requestDtos, categoryType);
     }
 }
